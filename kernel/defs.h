1775423168dac93156c3fdf8303f44eac75136bd
czh <jingzi_ccc>
2023-08-18 12:08:53 +0800

    ready to submit my lab

--- kernel/defs.h
+++ kernel/defs.h
@@ -8,6 +8,7 @@ struct spinlock;
 struct sleeplock;
 struct stat;
 struct superblock;
+struct ref_stru;
 
 // bio.c
 void            binit(void);
@@ -63,6 +64,10 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+int             kaddrefcnt(void *);
+int             krefcnt(void *);
+void*           cowalloc(pagetable_t, uint64);
+int             cowpage(pagetable_t, uint64);
 
 // log.c
 void            initlog(int, struct superblock*);
@@ -170,6 +175,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+pte_t*          walk(pagetable_t, uint64, int);
 
 // plic.c
 void            plicinit(void);
--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,10 +23,15 @@ struct {
   struct run *freelist;
 } kmem;
 
+struct ref_stru{
+  struct spinlock lock;
+  int cnt[PHYSTOP / PGSIZE];  // 引用计数
+}ref;
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&ref.lock, "ref");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,8 +40,11 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    ref.cnt[(uint64)p / PGSIZE] = 1;
     kfree(p);
+  }
+    
 }
 
 // Free the page of physical memory pointed at by v,
@@ -51,15 +59,22 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+  acquire(&ref.lock);
+  if(--ref.cnt[(uint64)pa / PGSIZE]==0){
+    release(&ref.lock);
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+    r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
+  }else{
+    release(&ref.lock);
+  }
+  
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -72,11 +87,79 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r){
     kmem.freelist = r->next;
+    acquire(&ref.lock);
+    ref.cnt[(uint64)r / PGSIZE] = 1; // 初始化为1
+    release(&ref.lock);
+  }
   release(&kmem.lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+}
+
+int cowpage(pagetable_t pagetable, uint64 va)
+{
+  if(va>=MAXVA){
+    return -1;
+  }
+  pte_t* pte = walk(pagetable, va, 0);
+  if((pte==0)||((*pte & PTE_V) == 0)||((*pte & PTE_RSW_COW)==0)){
+    return -1;
+  }
+  return 0;
+}
+
+void* cowalloc(pagetable_t pagetable, uint64 va)
+{
+  if(va % PGSIZE != 0){
+    return 0;
+  }
+  uint64 pa = walkaddr(pagetable, va);  // 获取对应的物理地址
+  if(pa==0){
+    return 0;
+  }
+  pte_t* pte = walk(pagetable, va, 0);  // 获取对应的PTE
+  
+  if(krefcnt((char*)pa)==1){// Only one process has a reference to this physical address, then just modify the corresponding PTE
+    *pte |= PTE_W; // Writable
+    *pte &= ~PTE_RSW_COW; // No ref
+    return (void*)pa;
+  }
+  else{ // Multiple processes have references to physical memory
+    // New pages need to be allocated and the contents of old pages copied
+    void* mem = kalloc();
+    if(mem == 0)
+      return 0;
+    
+    memmove(mem, (char*)pa, PGSIZE);
+    *pte &= ~PTE_V; // 清除PTE_V，否则在mappagges中会判定为remap
+
+     // 为新页面添加映射
+    if(mappages(pagetable,va,PGSIZE,(uint64)mem,(PTE_FLAGS(*pte)| PTE_W) & ~PTE_RSW_COW)!=0){
+      kfree(mem);
+      *pte |= PTE_V;
+      return 0; 
+    }
+     // 将原来的物理内存引用计数减1
+     kfree((char*)PGROUNDDOWN(pa));
+     return mem;
+  }
+}
+
+int krefcnt(void *pa)
+{
+  return ref.cnt[(uint64)pa/PGSIZE];
+}
+
+int kaddrefcnt(void *pa)
+{
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    return -1;
+  acquire(&ref.lock);
+  ++ref.cnt[(uint64)pa / PGSIZE];
+  release(&ref.lock);
+  return 0;
 }
